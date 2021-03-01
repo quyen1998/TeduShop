19fdee84b32e103d37fa6508ea5c2816565be27a
DESKTOP-LPNKME1\admin <nguyenquyen1998.net@gmail.com>
2021-03-01 10:44:19 +0700

    automapper for web

--- TeduShop.Data/Migrations/Configuration.cs
+++ TeduShop.Data/Migrations/Configuration.cs
@@ -1,9 +1,11 @@
 ﻿namespace TeduShop.Data.Migrations
 {
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using TeduShop.Model.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<TeduShop.Data.TeduShopDbContext>
     {
@@ -14,10 +16,8 @@
 
         protected override void Seed(TeduShop.Data.TeduShopDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
-        }
+            //List<PostCategory> listCategory = new List<PostCategory>();
+            //listCategory.Add(new PostCategory() { Name= "PostCategory",Alias= "Alias",CreatedBy= "CreatedBy",CreatedDate= DateTime.Now,Description= "Description",DisplayOrder=1,HomeFlag=null,Image=null,MetaDescription=null,MetaKeyword=null,Status=true,ParentID=1 });
+        } 
     }
 }
