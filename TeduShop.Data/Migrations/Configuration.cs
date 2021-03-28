d092b85044ecf60bbcf9386097a941f9b06cd8c2
DESKTOP-LPNKME1\admin <nguyenquyen1998.net@gmail.com>
2021-03-28 12:46:01 +0700

    list data product category

--- TeduShop.Data/Migrations/Configuration.cs
+++ TeduShop.Data/Migrations/Configuration.cs
@@ -17,33 +17,52 @@
         }
 
         protected override void Seed(TeduShop.Data.TeduShopDbContext context)
-        { //  This method will be called after migrating to the latest version.
+        {
 
-            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new TeduShopDbContext()));
+            CreateProductCategorySample(context);
+            //  This method will be called after migrating to the latest version.
 
-            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new TeduShopDbContext()));
+            //var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new TeduShopDbContext()));
 
-            var user = new ApplicationUser()
+            //var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new TeduShopDbContext()));
+
+            //var user = new ApplicationUser()
+            //{
+            //    UserName = "tedu",
+            //    Email = "tedu.international@gmail.com",
+            //    EmailConfirmed = true,
+            //    BirthDay = DateTime.Now,
+            //    FullName = "Technology Education"
+
+            //};
+
+            //manager.Create(user, "123654$");
+
+            //if (!roleManager.Roles.Any())
+            //{
+            //    roleManager.Create(new IdentityRole { Name = "Admin" });
+            //    roleManager.Create(new IdentityRole { Name = "User" });
+            //}
+
+            //var adminUser = manager.FindByEmail("tedu.international@gmail.com");
+
+            //manager.AddToRoles(adminUser.Id, new string[] { "Admin", "User" });
+
+        }
+        private void CreateProductCategorySample(TeduShop.Data.TeduShopDbContext context)
+        {
+            if (context.ProductCategories.Count() == 0)
             {
-                UserName = "tedu",
-                Email = "tedu.international@gmail.com",
-                EmailConfirmed = true,
-                BirthDay = DateTime.Now,
-                FullName = "Technology Education"
-
+                List<ProductCategory> listProductCategory = new List<ProductCategory>()
+            {
+                new ProductCategory() { Name="Điện lạnh",Alias="dien-lanh",Status=true },
+                 new ProductCategory() { Name="Viễn thông",Alias="vien-thong",Status=true },
+                  new ProductCategory() { Name="Đồ gia dụng",Alias="do-gia-dung",Status=true },
+                   new ProductCategory() { Name="Mỹ phẩm",Alias="my-pham",Status=true }
             };
-
-            manager.Create(user, "123654$");
-
-            if (!roleManager.Roles.Any())
-            {
-                roleManager.Create(new IdentityRole { Name = "Admin" });
-                roleManager.Create(new IdentityRole { Name = "User" });
+                context.ProductCategories.AddRange(listProductCategory);
+                context.SaveChanges();
             }
-
-            var adminUser = manager.FindByEmail("tedu.international@gmail.com");
-
-            manager.AddToRoles(adminUser.Id, new string[] { "Admin", "User" });
 
         }
     }
--- TeduShop.Web/Mappings/AutoMapperConfiguration.cs
+++ TeduShop.Web/Mappings/AutoMapperConfiguration.cs
@@ -11,6 +11,9 @@ namespace TeduShop.Web.Mappings
             Mapper.CreateMap<Post,PostViewModel>();
             Mapper.CreateMap<PostCategory, PostCategoryViewModel>();
             Mapper.CreateMap<Tag, TagViewModel>();
+            Mapper.CreateMap<ProductCategory, ProductCategoryViewModel>();
+            Mapper.CreateMap<Product, PostViewModel>();
+            Mapper.CreateMap<ProductTag, ProductTagViewModel>();
         }
     }
 }
