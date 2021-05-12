627d1dbf3f2fd1d3e529b6bc50be5bfcc83f869e
DESKTOP-LPNKME1\admin <nguyenquyen1998.net@gmail.com>
2021-05-12 10:13:41 +0700

    update form and make seo titlte

--- TeduShop.Web/app/shared/services/apiService.js
+++ TeduShop.Web/app/shared/services/apiService.js
@@ -9,11 +9,11 @@
     function apiService($http, notificationService) {
         return {
             get: get,
-            post: post
+            post: post,
+            put: put
         }
 
         function post(url, data, success, failure) {
-            debugger;
             $http.post(url, data).then(function (result) {
                 success(result);
             }, function (error) {
@@ -27,8 +27,21 @@
 
             });
         }
+        function put(url, data, success, failure) {
+            $http.put(url, data).then(function (result) {
+                success(result);
+            }, function (error) {
+                console.log(error.status)
+                if (error.status === 401) {
+                    notificationService.displayError('Authenticate is required.');
+                }
+                else if (failure != null) {
+                    failure(error);
+                }
+
+            });
+        }
         function get(url, params, success, failure) {
-            debugger;
             $http.get(url, params).then(function (result) {
                 success(result);
             }, function (error) {
