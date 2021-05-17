ec027742cd1b81a561f2336ba45e503a73fb655d
DESKTOP-LPNKME1\admin <nguyenquyen1998.net@gmail.com>
2021-05-17 11:09:25 +0700

    bindding category with footer

--- TeduShop.Data/Migrations/Configuration.cs
+++ TeduShop.Data/Migrations/Configuration.cs
@@ -7,6 +7,7 @@
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using TeduShop.Common;
     using TeduShop.Model.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<TeduShop.Data.TeduShopDbContext>
@@ -64,6 +65,13 @@
                 context.SaveChanges();
             }
 
+        }
+        private void CreateFooter(TeduShopDbContext context)
+        {
+            if (context.Footers.Count(x => x.ID == CommonConstants.DefaultFooterId) == 0)
+            {
+                string content = "";
+            }
         }
     }
 }
