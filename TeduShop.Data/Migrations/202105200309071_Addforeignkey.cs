0eda9f0b49a25e993f9336f1ec322491f3628960
DESKTOP-LPNKME1\admin <nguyenquyen1998.net@gmail.com>
2021-05-20 10:19:35 +0700

    Quản lý các sản phẩm theo Tags

---
+++ TeduShop.Data/Migrations/202105200309071_Addforeignkey.cs
@@ -0,0 +1,16 @@
+﻿namespace TeduShop.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Addforeignkey : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
